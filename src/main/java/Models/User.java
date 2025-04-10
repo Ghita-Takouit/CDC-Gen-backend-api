e1a7bb6cc5f668bc13d50aab3ab9ade7ab63f7e7
Ghita <takouitghita@gmail.com>
2025-04-10 11:04:21 +0100

    setup project

--- src/main/java/Models/User.java
+++ src/main/java/Models/User.java
@@ -1,4 +1,33 @@
 package Models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column
+    private String address;
+    
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    
+    @Column(nullable = false)
+    private String password;
 }
